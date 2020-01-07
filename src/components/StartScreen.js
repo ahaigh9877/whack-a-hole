1f977dcdbc86ab7e0813b84770ccc05626b83903
ahaigh9877 <alastairhaigh@me.com>
2020-01-07 14:25:47 +0100

    try loading screen again

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import ReactTimeout from "react-timeout";
 import passport from "../assets/start-screen/Passport.png";
-import Board3 from "../components/Board3";
 import { Link } from "react-router-dom";
 
 class StartScreen extends Component {
@@ -9,7 +8,8 @@ class StartScreen extends Component {
     super(props);
 
     this.state = {
-      seconds: 0
+      seconds: 0,
+      imageLoaded: false
     };
   }
 
@@ -19,42 +19,49 @@ class StartScreen extends Component {
     }));
   }
 
+  handleImageLoad() {
+    this.setState({ imageLoaded: true });
+  }
+
   componentDidMount() {
     console.log("componentDidMount");
     this.interval = this.props.setInterval(() => this.tick(), 1000);
   }
 
   render() {
-    if (this.state.seconds < 6) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <div className="startScreenContainer">
-          <div className="passportContainer">
-            <img className="passport" alt="passport" src={passport} />
-          </div>
-          {this.state.seconds > 8 && (
-            <div className="introTextContainer">
-              These horrible men want to take your TRAVEL RIGHTS away!
-              <br />
-              Don't let them get away with it...
-            </div>
-          )}
-          {this.state.seconds > 14 && (
-            <div className="bashABrexiterContainer">
-              <div className="bashABrexiter">
-                BASH
-                <br />A<br />
-                BREXITER!
-              </div>
-              <Link to={"/game"}>
-                <button className="BaBbutton">GO</button>
-              </Link>
-            </div>
-          )}
+    return (
+      <div className="startScreenContainer">
+        {this.state.imageLoaded ? null : <div>Loading...</div>}
+        <div className="passportContainer">
+          <img
+            onLoad={() => this.setState({ imageLoaded: true })}
+            style={this.state.imageLoaded ? {} : { display: "none" }}
+            className="passport"
+            alt="passport"
+            src={passport}
+          />
         </div>
-      );
-    }
+        {this.state.seconds > 8 && (
+          <div className="introTextContainer">
+            These horrible men want to take your TRAVEL RIGHTS away!
+            <br />
+            Don't let them get away with it...
+          </div>
+        )}
+        {this.state.seconds > 14 && (
+          <div className="bashABrexiterContainer">
+            <div className="bashABrexiter">
+              BASH
+              <br />A<br />
+              BREXITER!
+            </div>
+            <Link to={"/game"}>
+              <button className="BaBbutton">GO</button>
+            </Link>
+          </div>
+        )}
+      </div>
+    );
   }
 }
 
